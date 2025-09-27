c96cbbdd056e36ac92339244640581da302b6761
qiaosungithub <sqa23@mails.tsinghua.edu.cn>
2025-09-27 22:58:00 +0000

    nothing

--- rub.sh
+++ rub.sh
@@ -1,4 +1,4 @@
-MOUNT=kmh-nfs-us-mount
+MOUNT=kmh-nfs-ssd-us-mount
 # MOUNT=kmh-nfs-us-mount
 # MOUNT=kmh-nfs-ssd-eu-mount
 CMD="python prepare_dataset.py --config configs/default.py --imagenet_root /$MOUNT/data/imagenet --output_dir /$MOUNT/data/vae_cached_muvar_imagenet_zhh --overwrite --flip"
@@ -7,10 +7,20 @@ VM_NAME=kmh-tpuvm-v4-32-spot-yiyang3
 ZONE=us-central2-b
 # ZONE=us-central1-a
 
-HERE=/kmh-nfs-ssd-eu-mount/staging/sqa/sbsbsbsbsbsbssb/meanflow
+HERE=/kmh-nfs-ssd-us-mount/staging/sqa/sbsbsbsbsbsbssb/meanflow
 sudo rsync -av . $HERE --exclude='.git'
 sudo chmod 777 -R $HERE
 
-gcloud compute tpus tpu-vm ssh $VM_NAME --zone $ZONE --command "ls /kmh-nfs-ssd-eu-mount && echo Starting at \$(date); source /etc/profile; cd $HERE; $CMD; echo Finished at \$(date)" --worker=all
+MOUNT_DISK="
+sudo mkdir -p /kmh-nfs-ssd-us-mount
+sudo mount -o vers=3 10.97.81.98:/kmh_nfs_ssd_us /kmh-nfs-ssd-us-mount
+sudo chmod go+rw /kmh-nfs-ssd-us-mount
+"
 
-#gcloud compute tpus tpu-vm ssh $VM_NAME --zone $ZONE --command "ps -ef | grep prepare_dataset.py | grep -v grep | awk '{print \$2}'; echo Killed at \$(date)" --worker=all
+gcloud compute tpus tpu-vm ssh $VM_NAME --zone $ZONE --command "echo start at \$(date) && sudo apt update -y && sudo apt install nfs-common -y
+$MOUNT_DISK 
+ls /$MOUNT && echo done at \$(date)" --worker=all
+
+gcloud compute tpus tpu-vm ssh $VM_NAME --zone $ZONE --command "ls /kmh-nfs-ssd-us-mount && echo Starting at \$(date); source /etc/profile; cd $HERE; $CMD; echo Finished at \$(date)" --worker=all
+
+# gcloud compute tpus tpu-vm ssh $VM_NAME --zone $ZONE --command "ps -ef | grep prepare_dataset.py | grep -v grep | awk '{print \$2}'; echo Killed at \$(date)" --worker=all
